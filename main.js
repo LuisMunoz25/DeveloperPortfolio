da6c312d34ac3defff0f769d011cfdbd99db7f26
LuisMunoz25 <146249890+LuisMunoz25@users.noreply.github.com>
2024-03-11 04:11:17 -0400

    some details

--- main.js
+++ main.js
@@ -9,10 +9,12 @@ window.onload = () => {
 
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
+    
 
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
+        document.body.classList.toggle('no-scroll');
     });
 };
 
