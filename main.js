ff2b038c945940824c91fd38d1aaabbc2a2e4173
LuisMunoz25 <146249890+LuisMunoz25@users.noreply.github.com>
2024-03-10 20:28:02 -0400

    Folder Change

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    window.addEventListener('scroll', function (e) {
+        if (window.scrollY > 100) {
+            this.document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            this.document.querySelector("header").classList.remove('is-scrolling');
+        }
+    })
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+};
+
